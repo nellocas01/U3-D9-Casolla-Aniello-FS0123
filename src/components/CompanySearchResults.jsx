22ab8ca9384a2c297f8a08c94e2b1ea19672f18e
Nello Casolla <nellocasolla446@gmail.com>
2023-04-13 19:58:23 +0200

    finish

--- src/components/CompanySearchResults.jsx
+++ src/components/CompanySearchResults.jsx
@@ -1,44 +1,45 @@
-import { useEffect, useState } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import Job from './Job'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Job from "./Job";
+import { useParams } from "react-router-dom";
 
 const CompanySearchResults = () => {
-  const [jobs, setJobs] = useState([])
-  const params = useParams()
+  const [jobs, setJobs] = useState([]);
+  const params = useParams();
 
   const baseEndpoint =
-    'https://strive-benchmark.herokuapp.com/api/jobs?company='
+    "https://strive-benchmark.herokuapp.com/api/jobs?company=";
 
   useEffect(() => {
-    getJobs()
-  }, [])
+    getJobs();
+  }, []);
 
   const getJobs = async () => {
     try {
-      const response = await fetch(baseEndpoint + params.companyName)
+      const response = await fetch(baseEndpoint + params.companyName);
       if (response.ok) {
-        const { data } = await response.json()
-        setJobs(data)
+        const { data } = await response.json();
+        setJobs(data);
       } else {
-        alert('Error fetching results')
+        alert("Error fetching results");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <Container>
       <Row>
         <Col>
+          <h1 className="display-4">Job posting for: {params.company}</h1>
           {jobs.map((jobData) => (
             <Job key={jobData._id} data={jobData} />
           ))}
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default CompanySearchResults
+export default CompanySearchResults;
